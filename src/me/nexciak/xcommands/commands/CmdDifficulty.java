6eef9fae202359cf130809fb45b6291d604de862
Nexciak <nexti.vextor@gmail.com>
2016-08-14 00:19:32 +0200

    Update komend Creative i Difficulty

--- src/me/nexciak/xcommands/commands/CmdDifficulty.java
+++ src/me/nexciak/xcommands/commands/CmdDifficulty.java
@@ -13,15 +13,9 @@ public class CmdDifficulty implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("defficulty")){
+		if(cmd.getName().equalsIgnoreCase("difficulty")){
 			if(sender.hasPermission("xcommands.difficulty")){
-				
-				args[0] = args[0].replaceAll(String.valueOf(0), "Peaceful");
-				args[0] = args[0].replaceAll(String.valueOf(1), "Easy");
-				args[0] = args[0].replaceAll(String.valueOf(2), "Normal");
-				args[0] = args[0].replaceAll(String.valueOf(3), "Hard");
-				
-				if(!(args.length == 1)){
+				if(args.length == 0){
 					sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aUzyj &c/dif <tryb>"));
 					sender.sendMessage(StringUtils.colored("&c&m----------------------------"));
 					sender.sendMessage(StringUtils.colored("&3» &aPeaceful &3| &70"));
@@ -32,24 +26,28 @@ public class CmdDifficulty implements CommandExecutor {
 				}
 				if(args.length == 1){
 					if(args[0].equalsIgnoreCase("peaceful") || args[0].equalsIgnoreCase("0")){
-					World w = ((Player) sender).getWorld();
-					w.setDifficulty(Difficulty.PEACEFUL);
-					sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
+						args[0] = args[0].replaceAll(String.valueOf(0), "Peaceful");
+						World w = ((Player) sender).getWorld();
+						w.setDifficulty(Difficulty.PEACEFUL);
+						sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
 					}
 					if(args[0].equalsIgnoreCase("easy") || args[0].equalsIgnoreCase("1")){
-					World w = ((Player) sender).getWorld();
-					w.setDifficulty(Difficulty.EASY);
-					sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
+						args[0] = args[0].replaceAll(String.valueOf(1), "Easy");
+						World w = ((Player) sender).getWorld();
+						w.setDifficulty(Difficulty.EASY);
+						sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
 					}
 					if(args[0].equalsIgnoreCase("normal") || args[0].equalsIgnoreCase("2")){
-					World w = ((Player) sender).getWorld();
-					w.setDifficulty(Difficulty.NORMAL);
-					sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
+						args[0] = args[0].replaceAll(String.valueOf(2), "Normal");
+						World w = ((Player) sender).getWorld();
+						w.setDifficulty(Difficulty.NORMAL);
+						sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
 					}
 					if(args[0].equalsIgnoreCase("hard") || args[0].equalsIgnoreCase("3")){
-					World w = ((Player) sender).getWorld();
-					w.setDifficulty(Difficulty.HARD);
-					sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
+						args[0] = args[0].replaceAll(String.valueOf(3), "Hard");
+						World w = ((Player) sender).getWorld();
+						w.setDifficulty(Difficulty.HARD);
+						sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aZmieniono na tryb &c" + args[0]));
 					}
 				} else{
 					sender.sendMessage(StringUtils.colored("&6Difficulty &3| &aUzyj &c/dif <tryb>"));
--- src/me/nexciak/xcommands/commands/CmdGamemode.java
+++ src/me/nexciak/xcommands/commands/CmdGamemode.java
@@ -15,12 +15,6 @@ public class CmdGamemode implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String arg2, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("gm")){
 			if(sender.hasPermission("xcommands.gm")){
-				
-				args[0] = args[0].replaceAll(String.valueOf(0), "Survival");
-				args[0] = args[0].replaceAll(String.valueOf(1), "Creative");
-				args[0] = args[0].replaceAll(String.valueOf(2), "Adventure");
-				args[0] = args[0].replaceAll(String.valueOf(3), "Spectator");
-				
 				if(args.length == 0){
 					sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUzyj &c/gm <tryb>"));
 					sender.sendMessage(StringUtils.colored("&c&m----------------------------"));
@@ -33,21 +27,25 @@ public class CmdGamemode implements CommandExecutor {
 				if(args.length == 1){
 					if(sender instanceof Player){
 						if(args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("0")){
+							args[0] = args[0].replaceAll(String.valueOf(0), "Survival");
 							((Player) sender).setGameMode(GameMode.SURVIVAL);
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0]));
 							return true;
 						}
 						if(args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("1")){
+							args[0] = args[0].replaceAll(String.valueOf(1), "Creative");
 							((Player) sender).setGameMode(GameMode.CREATIVE);
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0]));
 							return true;
 						}
 						if(args[0].equalsIgnoreCase("adventure") || args[0].equalsIgnoreCase("2")){
+							args[0] = args[0].replaceAll(String.valueOf(2), "Adventure");
 							((Player) sender).setGameMode(GameMode.ADVENTURE);
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0]));
 							return true;
 						}
 						if(args[0].equalsIgnoreCase("spectator") || args[0].equalsIgnoreCase("3")){
+							args[0] = args[0].replaceAll(String.valueOf(3), "Spectator");
 							((Player) sender).setGameMode(GameMode.SPECTATOR);
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0]));
 							return true;
@@ -64,24 +62,28 @@ public class CmdGamemode implements CommandExecutor {
 						}
 						Player p = Bukkit.getPlayer(args[1]);
 						if(args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("0")){
+							args[0] = args[0].replaceAll(String.valueOf(0), "Survival");
 							p.setGameMode(GameMode.SURVIVAL);
 							p.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &aprzez &9" + sender.getName()));
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &adla &9" + sender.getName()));
 							return true;
 						}
 						if(args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("1")){
+							args[0] = args[0].replaceAll(String.valueOf(1), "Creative");
 							p.setGameMode(GameMode.CREATIVE);
 							p.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &aprzez &9" + sender.getName()));
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &adla &9" + sender.getName()));
 							return true;
 						}
 						if(args[0].equalsIgnoreCase("adventure") || args[0].equalsIgnoreCase("2")){
+							args[0] = args[0].replaceAll(String.valueOf(2), "Adventure");
 							p.setGameMode(GameMode.ADVENTURE);
 							p.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &aprzez &9" + sender.getName()));
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &adla &9" + sender.getName()));
 							return true;
 						}
 						if(args[0].equalsIgnoreCase("spectator") || args[0].equalsIgnoreCase("3")){
+							args[0] = args[0].replaceAll(String.valueOf(3), "Spectator");
 							p.setGameMode(GameMode.SPECTATOR);
 							p.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &aprzez &9" + sender.getName()));
 							sender.sendMessage(StringUtils.colored("&6GameMode &3| &aUstawiono tryb &c" + args[0] + " &adla &9" + sender.getName()));
