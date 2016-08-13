0f87e4d7c95b42083d34d665105be1da7a7514b3
Nexciak <nexti.vextor@gmail.com>
2016-08-13 21:02:38 +0200

    Update komendy kill

--- src/me/nexciak/xcommands/Main.java
+++ src/me/nexciak/xcommands/Main.java
@@ -16,7 +16,7 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getServer().getConsoleSender().sendMessage(StringUtils.colored("&6xCommands &3| &aWlaczanie..."));
-        Bukkit.getServer().getConsoleSender().sendMessage(StringUtils.colored("&c&m----------------------"));
+        Bukkit.getServer().getConsoleSender().sendMessage(StringUtils.colored("&c&m------------------------"));
         getCommand("kill").setExecutor(new CmdKill());
     }
 }
--- src/me/nexciak/xcommands/commands/CmdKill.java
+++ src/me/nexciak/xcommands/commands/CmdKill.java
@@ -12,22 +12,31 @@ public class CmdKill implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String args2, String[] args) {
         if(cmd.getName().equalsIgnoreCase("kill")){
+            if(args.length == 0){
+        	if(!(sender instanceof Player)){
+        		sender.sendMessage(StringUtils.colored("&4Blad &3| &cMusisz byc na serwerze"));
+        		return true;
+        		}
+        	}
             if(sender.hasPermission("xcommands.kill")){
                 if(args.length == 0){
+                	if(sender instanceof Player){
                     Player p = (Player) sender;
                     p.setHealth(0);
                     p.sendMessage(StringUtils.colored("&6Kill &3| &aZabiles sie"));
+                	} else{
+                		sender.sendMessage(StringUtils.colored("&4Blad &3| &cMusisz byc na serwerze"));
+                	}
                 }
                 if(args.length == 1){
-                    Player p = (Player) sender;
                     Player cel = Bukkit.getPlayer(args[0]);
                     if(!(Bukkit.getPlayer(args[0]) != null)){
-                        p.sendMessage(StringUtils.colored("&4Blad &3| &cGracza &a" + args[0] + " &cnie ma na serwerze"));
+                        sender.sendMessage(StringUtils.colored("&4Blad &3| &cGracza &a" + args[0] + " &cnie ma na serwerze"));
                         return true;
                     }
                     cel.setHealth(0);
-                    p.sendMessage(StringUtils.colored("&6Kill &3| &aZabiles gracza &c" + cel.getName()));
-                    cel.sendMessage(StringUtils.colored("&6Kill &3| &cZostales zabity przez &a" + p.getName()));
+                    sender.sendMessage(StringUtils.colored("&6Kill &3| &aZabiles gracza &c" + cel.getName()));
+                    cel.sendMessage(StringUtils.colored("&6Kill &3| &cZostales zabity przez &a" + sender.getName()));
                 }
             } else{
                 sender.sendMessage(StringUtils.colored("&4Blad &3| &cNie masz uprawnien"));
